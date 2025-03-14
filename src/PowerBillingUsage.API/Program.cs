5a5641928f514a9d83acc78b53205f7bdf1f876e
Eyas Wannous <eyaswannous@gmail.com>
2025-03-14 20:17:59 +0300

    Update

--- src/PowerBillingUsage.API/Program.cs
+++ src/PowerBillingUsage.API/Program.cs
@@ -4,6 +4,8 @@ using PowerBillingUsage.Infrastructure.EntityFramework;
 using PowerBillingUsage.Core.ApplicationContracts;
 using Microsoft.EntityFrameworkCore;
 using PowerBillingUsage.Core.AppServices;
+using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,10 +18,37 @@ builder.Services.AddDbContext<PowerBillingUsageDbContext>(options =>
     options.UseInMemoryDatabase("PowerBillingUsageDb")
 );
 
+//builder.Services.AddRateLimiter(options =>
+//{
+//    options.AddPolicy("FixedWindow", context =>
+//        RateLimitPartition.GetFixedWindowLimiter(
+//            partitionKey: context.Request.Path,
+//            factory: _ => new FixedWindowRateLimiterOptions
+//            {
+//                AutoReplenishment = true,
+//                PermitLimit = 10,
+//                Window = TimeSpan.FromSeconds(10)
+//            }
+//        )
+//    );
+//});
+
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("customPolicy", opt =>
+    {
+        opt.PermitLimit = 4;
+        opt.Window = TimeSpan.FromSeconds(12);
+        opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        opt.QueueLimit = 2;
+    });
+});
+
 builder.Services.AddScoped<IBillRepository, BillRepository>();
 builder.Services.AddScoped<IBillDetailRepository, BillDetailRepository>();
 builder.Services.AddScoped<ITierRepository, TierRepository>();
 builder.Services.AddScoped<IBillingCalculatorAppService, BillingCalculatorAppService>();
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
@@ -30,7 +59,10 @@ builder.Services.AddCors(options =>
     });
 });
 
+
 var app = builder.Build();
+
+app.UseRateLimiter();
 
 app.MapDefaultEndpoints();
 
