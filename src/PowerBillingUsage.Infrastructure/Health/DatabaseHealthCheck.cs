9bb2698fb07c6856d58c9394462208e056e7885e
Eyas Wannous <eyaswannous@gmail.com>
2025-03-24 21:49:43 +0300

    Add HealthCheck for redis and postgres and PowerBillingUsageDbContext and a custom one that I have made but it is for learning purpose

---
+++ src/PowerBillingUsage.Infrastructure/Health/DatabaseHealthCheck.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using PowerBillingUsage.Infrastructure.EntityFramework;
+using System.Data.Common;
+
+namespace PowerBillingUsage.Infrastructure.Health;
+
+public sealed class DatabaseHealthCheck(PowerBillingUsageDbContext dbContext) : IHealthCheck
+{
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            await dbContext.Database.OpenConnectionAsync(cancellationToken);
+
+            return HealthCheckResult.Healthy();
+        }
+        catch (Exception ex)
+        {
+            return HealthCheckResult.Unhealthy(exception: ex);
+        }
+        finally
+        {
+
+            await dbContext.Database.CloseConnectionAsync();
+        }
+    }
+}
